a89350d19e1304ac1636ff83ebad3047077bcef7
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2026-02-16 16:36:58 +0530

    edited server file for deploy

--- backend/server.js
+++ backend/server.js
@@ -41,4 +41,4 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.use(dbMiddleware);
-app.listen(PORT, () => console.log("Server Started at port : ", PORT));
+app.listen(PORT, '0.0.0.0', () => console.log("Server Started at port : ", PORT));
